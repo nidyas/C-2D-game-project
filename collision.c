8ae5e03256ba62c37dca655e99b3ea1004816348
nidyas <foboki111@hotmail.com>
2020-04-15 07:27:12 -0700

    collision

---
+++ collision.c
@@ -0,0 +1,47 @@
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include "SDL/SDL_ttf.h"
+#include "Collision.h"
+
+int testCollision(SDL_Rect obj1, SDL_Rect obj2, int * typeCollision)
+{
+    int leftA, leftB;
+    int rightA, rightB;
+    int topA, topB;
+    int bottomA, bottomB;
+
+    leftA = obj1.x;
+    rightA = obj1.x + obj1.w;
+    topA = obj1.y;
+    bottomA = obj1.y + obj1.h;
+
+    leftB = obj2.x;
+    rightB = obj2.x + obj2.w;
+    topB = obj2.y;
+    bottomB = obj2.y + obj2.h;
+
+    if( bottomA <= topB )
+    {
+        *typeCollision = BOTTOM;return 1;
+    }
+
+
+    if( topA >= bottomB )
+    {
+        *typeCollision = TOP;return 1;
+    }
+
+    if( rightA <= leftB )
+    {
+        *typeCollision = RIGHT;return 1;
+    }
+
+    if( leftA >= rightB )
+    {
+        *typeCollision = LEFT;return 1;
+    }
+    return 0;
+}
+
+
---
+++ collision.h
@@ -0,0 +1,18 @@
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include "SDL/SDL_ttf.h"
+
+
+
+int testCollision(SDL_Rect obj1, SDL_Rect obj2, int * typeCollision);
+
+
+
+
+
+
+#endif // FONCTIONS_H_INCLUDED
+
